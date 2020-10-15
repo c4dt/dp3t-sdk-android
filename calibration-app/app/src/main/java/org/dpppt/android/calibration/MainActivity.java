329c4e4cb283564ec64816436b4074a028ed3f94
Christian Grigis <christian.grigis@epfl.ch>
2020-10-15 17:25:57 +0200

    calibration: code changes

--- calibration-app/app/src/main/java/org/dpppt/android/calibration/MainActivity.java
+++ calibration-app/app/src/main/java/org/dpppt/android/calibration/MainActivity.java
@@ -9,10 +9,17 @@
  */
 package org.dpppt.android.calibration;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
@@ -24,10 +31,35 @@ import org.dpppt.android.sdk.DP3T;
 
 public class MainActivity extends AppCompatActivity {
 
+	private ActivityResultLauncher<String[]> requestPermissionLauncher =
+			registerForActivityResult(new ActivityResultContracts.RequestMultiplePermissions(), isGranted -> {
+				Boolean locationGranted = isGranted.get(Manifest.permission.ACCESS_FINE_LOCATION);
+
+				if (locationGranted == null || !locationGranted) {
+					new AlertDialog.Builder(this)
+						.setTitle("Missing required permission")
+						.setMessage("This application requires access to the device's location" +
+									" and will now close.")
+						.setIcon(R.drawable.ic_error)
+						.setCancelable(false)
+						.setPositiveButton("OK", (view, arg) -> {
+							finishAndRemoveTask();
+						}).create().show();
+				}
+			});
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+
+		if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) !=
+				PackageManager.PERMISSION_GRANTED) {
+			requestPermissionLauncher.launch(new String[]{
+					Manifest.permission.ACCESS_FINE_LOCATION,
+					Manifest.permission.ACCESS_BACKGROUND_LOCATION,
+			});
+		}
 
 		setupNavigationView();
 
